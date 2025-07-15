6f51a624e2e6e931ccc5faa365725d96a3fcbccc
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-07-15 14:41:47 -0500

    ✨ feat: Edit effective balance

--- src/js/modules/selectors.js
+++ src/js/modules/selectors.js
@@ -2,12 +2,20 @@
 export const sidebar = document.querySelector(".nav");
 export const sidebarMenu = document.querySelector("#sidebar__menu")
 
-//* Credit card container
+//* Modals Container
+export const modalsContainer = document.querySelector(".modals")
+
+//* Credit card
 export const creditCardsContainer = document.querySelector(".credit__cards");
 export const addCreditCardContainer = document.querySelector(".credit__card--add")
 
+//* Effective
+export const effectiveCard = document.querySelector(".card__effective");
+export const effectiveBalance = document.querySelector(".effective__balance");
+export const effectiveModalTemplate = document.querySelector("#modal-effective-template");
+
 //* Forms
-export const form = document.querySelector("#form");
+export const cardForm = document.querySelector("#card-form");
 
 export const closeButtons = document.querySelectorAll(".form__btn--close");
 
