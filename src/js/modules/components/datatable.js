26075b82ff8aea841870f8442734a184ece8681b
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-07-24 21:37:35 -0500

    🐛 fix: Datatable PDF export

--- src/js/modules/components/datatable.js
+++ src/js/modules/components/datatable.js
@@ -1,10 +1,12 @@
 import pdfmake from 'pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts.js'
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-buttons-dt';
 import 'datatables.net-buttons/js/buttons.html5.mjs';
 import 'datatables.net-responsive-dt';
 import { formatTableTransactions } from '../../transactions.js';
- 
+
+pdfmake.vfs = pdfFonts?.default?.pdfMake?.vfs || pdfFonts?.pdfMake?.vfs;
 DataTable.Buttons.pdfMake(pdfmake);
 
 export async function initDatatable() {
@@ -62,7 +64,36 @@ export async function initDatatable() {
             {
                 extend: 'pdfHtml5',
                 text: '<i class="ri-file-pdf-2-fill"></i>',
-                className: 'transaction__pdf'
+                className: 'transaction__pdf',
+                customize: doc => {
+                    //Title
+                    doc.content[0].text = "Reporte de Transacciones";
+                    doc.content[0].fontSize = 20;
+                    doc.content[0].bold = true;
+                    doc.content[0].alignment = 'center';
+
+                    //Table Content
+                    doc.content[1].alignment = 'center';
+
+                    //Table header
+                    doc.styles.tableHeader = {
+                        bold: true,
+                        fontSize: 12,
+                        color: 'white',
+                        fillColor: '#0ACF83',
+                        alignment: 'center',
+                    };
+                },
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4, 5],
+                    format: {
+                        body: function (data) {
+                            const div = document.createElement("div");
+                            div.innerHTML = data;
+                            return div.textContent.trim();
+                        }
+                    }
+                }
             }
         ],
         language: {
